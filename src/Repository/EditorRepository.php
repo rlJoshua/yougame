06516dc33a1cc25e1c3cd0a39d2d42166b74f485
rdaoud <ryan.daoud07@gmail.com>
2020-01-17 17:51:31 +0100

    Implementation Editor
    Création d'éditeur et affichage des éditeurs

---
+++ src/Repository/EditorRepository.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Editor;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Common\Persistence\ManagerRegistry;
+
+/**
+ * @method Editor|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Editor|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Editor[]    findAll()
+ * @method Editor[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class EditorRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Editor::class);
+    }
+
+    // /**
+    //  * @return Editor[] Returns an array of Editor objects
+    //  */
+    /*
+    public function findByExampleField($value)
+    {
+        return $this->createQueryBuilder('e')
+            ->andWhere('e.exampleField = :val')
+            ->setParameter('val', $value)
+            ->orderBy('e.id', 'ASC')
+            ->setMaxResults(10)
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+    */
+
+    /*
+    public function findOneBySomeField($value): ?Editor
+    {
+        return $this->createQueryBuilder('e')
+            ->andWhere('e.exampleField = :val')
+            ->setParameter('val', $value)
+            ->getQuery()
+            ->getOneOrNullResult()
+        ;
+    }
+    */
+}
